c7345cc9b1ad800ca13df3681b3305ce975b5b35
bogdanoancea <bogdan.oancea@gmail.com>
2022-08-08 22:58:55 +0300

    added negative binomial

---
+++ R/negbinom.R
@@ -0,0 +1,51 @@
+
+#' @param n The finitization order. It should be an integer > 0.
+#' @param p
+#' @param N
+#' @param val The value of the variable for which the probability density function is computed. If NULL, a data frame containing
+#' all possible values, i.e. {0 .. n}, and the corresponding probabilities is returned.
+#' @export
+dnegbinom <- function(n, p, k, val = NULL) {
+    if (!is.null(val)) {
+        return(c_dnegbinom(n, p, k, val))
+    } else {
+        df <- data.frame(matrix(ncol = 2, nrow = 0))
+
+        for (i in 0:n) {
+            r <- c_dnegbinom(n, p, k, i)
+            df <- rbind(df, c(i, r))
+        }
+        x <- c("val", "prob")
+        colnames(df) <- x
+        return(df)
+    }
+}
+
+#' @param n The finitization order. It should be an integer > 0.
+#' @export
+getNegativeBinomialMFPS <- function(n, k) {
+    fg <- function(p) { "x" }
+    body(fg)[[2]] <- parse(text = MFPS_negbinom_pdf(n, k))[[1]]
+    solutions <- rootSolve::uniroot.all(fg, c(0,1), n = 10^7, tol = .Machine$double.eps)
+    UL = solutions[length(solutions)]
+    LL = solutions[length(solutions) - 1]
+    return(c(LL, UL))
+}
+
+#' @param n The finitization order. It should be an integer > 0.
+#' @param val The value of the variable for which the probability density function is printed. If NULL, this function prints the
+#' pdf for all possible values, i.e. {0 .. n}.
+#' @param latex If TRUE, a string representation of the pdf formatted in Latex format is printed, otherwise it prints
+#' the string representation of the pdf as an R expression.
+#' @export
+printFinitizedNegativeBinomialDensity <- function(n, k, val = NULL, latex = FALSE)  {
+    if(!is.null(val)) {
+        x <- c_printFinitizedBinomialDensity(n, k, val, latex)
+    } else {
+        cat(paste0("X", "\t", "pdf\n"))
+        for (i in 0:n) {
+            cat(paste0(i,":", '\t'))
+            x <- c_printFinitizedBinomialDensity(n, k, i, latex)
+        }
+    }
+}
