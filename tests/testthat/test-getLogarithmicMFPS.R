198f63a9aa1de4cc0e3e2678881e6a92f068f1da
bogdan.oancea@gmail.com <bogdan.oancea@gmail.com>
2022-08-10 18:03:06 +0300

    tests for logarithmic mfps

---
+++ tests/testthat/test-getLogarithmicMFPS.R
@@ -0,0 +1,16 @@
+test_that("logarithmic MFPS", {
+    expect_equal(round(getLogarithmicMFPS(2), 7), c(0.0000000, 0.4404231) )
+})
+
+test_that("logarithmic MFPS 1", {
+    expect_equal(round(getLogarithmicMFPS(3), 7), c(0.0000081, 0.3115801) )
+})
+
+test_that("logarithmic MFPS 2", {
+    expect_equal(round(getLogarithmicMFPS(4), 7), c(0.0001775, 0.2397019) )
+})
+
+
+test_that("logarithmic MFPS 2", {
+    expect_equal(round(getLogarithmicMFPS(7), 4), c(0.0085, 0.1406) )
+})
