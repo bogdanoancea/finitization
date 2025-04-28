9f5f203b40817f3956442a5009c647d57cfd971a
bogdanoancea <bogdan.oancea@gmail.com>
2025-04-28 21:07:38 +0300

    cout

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -1,26 +1,10 @@
 #include <Rcpp.h>
 #include <iostream>
-#include <fstream>
+#include <sstream>  // use stringstream instead of file stream
 
-// Suppresses cout and cerr output
-void silence_ginac_output() {
-#ifdef _WIN32
-    static std::ofstream null_stream("NUL");
-    if (null_stream.is_open()) {
-        std::cout.rdbuf(null_stream.rdbuf());
-        std::cerr.rdbuf(null_stream.rdbuf());
-    }
-#else
-    static std::ofstream null_stream("/dev/null");
-    if (null_stream.is_open()) {
-        std::cout.rdbuf(null_stream.rdbuf());
-        std::cerr.rdbuf(null_stream.rdbuf());
-    }
-#endif
-}
-
-// Exported function to R
 // [[Rcpp::export]]
 void initialize_finitization() {
-    silence_ginac_output();
+    static std::ostringstream null_stream;
+    std::cout.rdbuf(null_stream.rdbuf());
+    std::cerr.rdbuf(null_stream.rdbuf());
 }
