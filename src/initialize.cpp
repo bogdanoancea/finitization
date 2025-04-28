53184a56515b05d060acadaa7ac034ac03f48f1d
bogdanoancea <bogdan.oancea@gmail.com>
2025-04-28 21:01:46 +0300

    cout

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -2,24 +2,20 @@
 #include <iostream>
 #include <fstream>
 
-// Anonymous namespace (local to file)
-namespace {
-#ifdef _WIN32
-std::ofstream null_stream("NUL");
-#else
-    std::ofstream null_stream("/dev/null");
-#endif
-}
-
 // Suppresses cout and cerr output
 void silence_ginac_output() {
 #ifdef _WIN32
-    std::ofstream null_stream_win("NUL");
-    std::cout.rdbuf(null_stream_win.rdbuf());
-    std::cerr.rdbuf(null_stream_win.rdbuf());
+    static std::ofstream null_stream("NUL");
+    if (null_stream.is_open()) {
+        std::cout.rdbuf(null_stream.rdbuf());
+        std::cerr.rdbuf(null_stream.rdbuf());
+    }
 #else
-    std::cout.rdbuf(null_stream.rdbuf());
-    std::cerr.rdbuf(null_stream.rdbuf());
+    static std::ofstream null_stream("/dev/null");
+    if (null_stream.is_open()) {
+        std::cout.rdbuf(null_stream.rdbuf());
+        std::cerr.rdbuf(null_stream.rdbuf());
+    }
 #endif
 }
 
