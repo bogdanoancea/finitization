f85600e8717c710c5e4e1a5e27dd537df6f8ba25
bogdanoancea <bogdan.oancea@gmail.com>
2025-10-29 05:30:55 +0200

    request testhtat in DESC, guard it in testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,7 @@
-library(testthat)
-library(finitization)
-
-test_check("finitization")
+if (requireNamespace("testthat", quietly = TRUE)) {
+    library(testthat)
+    library(finitization)
+    test_check("finitization")
+} else {
+    message("Skipping tests: 'testthat' not installed in this build environment.")
+}
